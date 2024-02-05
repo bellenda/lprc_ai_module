a44a95e1aa10c13ac6e236fb7f5c1e2faf2cf3e7
bellenda <92037900+bellenda@users.noreply.github.com>
2024-02-05 01:23:06 -0500

    Delete pc_dev/scratchpad.py
    removed redundant file

--- pc_dev/scratchpad.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-#Load a pretrained model
-model = YOLO('yolov8n.pt')
-
-#Run inference on the source
-results = model(source="retail_stock1.mp4", show=True, conf=0.4, save=True) #generator of Results objects
